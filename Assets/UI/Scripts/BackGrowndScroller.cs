be7b5d27a3b84db446093bcfe0b2d664be0f4020
Arthur <50921642+120arthur@users.noreply.github.com>
2020-08-10 19:54:25 -0300

    Coloquei as perguntas.
    Coloquei as perguntas no quiz.

---
+++ Assets/UI/Scripts/BackGrowndScroller.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class BackGrowndScroller : MonoBehaviour
+{
+    private RawImage backGrownd;
+    // Start is called before the first frame update
+    void Awake()
+    {
+        backGrownd = GetComponent<RawImage>(); 
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        backGrownd.uvRect = new Rect(backGrownd.uvRect.x, backGrownd.uvRect.y+Time.deltaTime*0.006f, backGrownd.uvRect.width, backGrownd.uvRect.height);
+    }
+}
