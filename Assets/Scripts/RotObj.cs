6270b50c65fa06815fd8303ac9323fb3a3168a13
Arthur <50921642+120arthur@users.noreply.github.com>
2020-08-21 19:17:03 -0300

    Adicionei as fontes

---
+++ Assets/Scripts/RotObj.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotObj : MonoBehaviour
+{
+    float rotSpeed = 150;
+
+    void OnMouseDrag()
+    {
+        float rotX = Input.GetAxis("Mouse X") * rotSpeed * Mathf.Deg2Rad;
+        float rotY = Input.GetAxis("Mouse Y") * rotSpeed * Mathf.Deg2Rad;
+
+        transform.Rotate(Vector3.up, -rotX, Space.World);
+        transform.Rotate(Vector3.right, rotY, Space.World);
+
+        /*
+         * if you move your camera to a different angle this won't work, so you can use:
+         * "transform.Rotate(Camera.main.up, -rotX, Space.World);"
+         */
+    }
+}
