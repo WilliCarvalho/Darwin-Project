a8fe385299b06849e615a4161ed6f52e7e3ae50e
Rafael <61071394+RadialLMosss@users.noreply.github.com>
2020-08-21 19:39:23 -0300

    Adicionei alguns sons
    Nos UI prefabs
    e começei a fazer o sound manager das músicas de fundo

---
+++ Assets/Scripts/SoundManager.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoundManager : MonoBehaviour
+{
+    AudioClip bgm1;
+    AudioClip bgm2;
+    AudioSource audioSource;
+
+    void Mute()
+    {
+
+    }
+
+    void Play_1()
+    {
+        audioSource.clip = bgm1;
+    }
+
+    void Play_2()
+    {
+        audioSource.clip = bgm1;
+    }
+}
